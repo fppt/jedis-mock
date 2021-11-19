c9a726d43b12a698627375437a940a5383aa4502
Александр Муратиди <userbkb@MacBook-Pro-Aleksandr.local>
2021-11-20 02:24:39 +0300

    persist operation support

--- src/main/java/com/github/fppt/jedismock/operations/RO_pexpire.java
+++ src/main/java/com/github/fppt/jedismock/operations/RO_pexpire.java
@@ -11,12 +11,9 @@ import static com.github.fppt.jedismock.Utils.convertToLong;
 
 @RedisCommand("pexpire")
 class RO_pexpire extends AbstractRedisOperation {
-    private final List<String> additionalParams;
 
     RO_pexpire(RedisBase base, List<Slice> params) {
         super(base, params);
-        additionalParams = params()
-                .stream().skip(2).map(Slice::toString).collect(Collectors.toList());
     }
 
     long getValue(List<Slice> params){
@@ -24,36 +21,7 @@ class RO_pexpire extends AbstractRedisOperation {
     }
 
     Slice response() {
-        Slice key = params().get(0);
-        long value = getValue(params());
-
-        Slice old = base().getSlice(key);
-        if (nx() && old != null || xx() && old == null) {
-            return Response.integer(0);
-        }
-
-        long oldTTL = base().getTTL(key);
-        if (gt() &&  (oldTTL == -1 || oldTTL >= value) || lt() && oldTTL != -1 && oldTTL <= value) {
-            return Response.integer(0);
-        }
-
-        return Response.integer(base().setTTL(key, value));
-    }
-
-    private boolean nx() {
-        return additionalParams.stream().anyMatch("nx"::equalsIgnoreCase);
-    }
-
-    private boolean xx() {
-        return additionalParams.stream().anyMatch("xx"::equalsIgnoreCase);
-    }
-
-    private boolean gt() {
-        return additionalParams.stream().anyMatch("gt"::equalsIgnoreCase);
-    }
-
-    private boolean lt() {
-        return additionalParams.stream().anyMatch("lt"::equalsIgnoreCase);
+        return Response.integer(base().setTTL(params().get(0), getValue(params())));
     }
 
 }
