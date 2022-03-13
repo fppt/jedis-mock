8072b7e816e9c14017a4a8e1424da33c1faa530c
mdvorchik <71084918+mdvorchik@users.noreply.github.com>
2022-03-13 22:17:41 +0300

    zrange/zrevrange support (#59)
    * zrevrange support
    
    * fix ZRange and add REV support
    
    Co-authored-by: Ivan Ponomarev <iponomarev@mail.ru>

--- src/main/java/com/github/fppt/jedismock/operations/sortedsets/ZRange.java
+++ src/main/java/com/github/fppt/jedismock/operations/sortedsets/ZRange.java
@@ -1,14 +1,16 @@
 package com.github.fppt.jedismock.operations.sortedsets;
 
 import com.github.fppt.jedismock.datastructures.RMHMap;
+import com.github.fppt.jedismock.datastructures.Slice;
 import com.github.fppt.jedismock.operations.AbstractRedisOperation;
 import com.github.fppt.jedismock.operations.RedisCommand;
 import com.github.fppt.jedismock.server.Response;
-import com.github.fppt.jedismock.datastructures.Slice;
 import com.github.fppt.jedismock.storage.RedisBase;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,6 +20,11 @@ import static com.github.fppt.jedismock.Utils.convertToInteger;
 class ZRange extends AbstractRedisOperation {
 
     private static final String WITH_SCORES = "WITHSCORES";
+    private static final String IS_REV = "REV";
+
+    private static final Comparator<Map.Entry<Slice, Double>> zRangeComparator = Comparator
+            .comparingDouble((ToDoubleFunction<Map.Entry<Slice, Double>>) Map.Entry::getValue)
+            .thenComparing(Map.Entry::getKey);
 
     ZRange(RedisBase base, List<Slice> params) {
         super(base, params);
@@ -50,15 +57,28 @@ class ZRange extends AbstractRedisOperation {
             end = map.size() - 1;
         }
 
-        final boolean withScores = params().size() == 4 && WITH_SCORES.equalsIgnoreCase(params().get(3).toString());
+        boolean withScores = false;
+        boolean isRev = false;
+
+        for (Slice param : params()) {
+            if (WITH_SCORES.equalsIgnoreCase(param.toString())){
+                withScores = true;
+            }
+            if (IS_REV.equalsIgnoreCase(param.toString())){
+                isRev = true;
+            }
+        }
+
+        boolean finalWithScores = withScores;
         List<Slice> values = map.entrySet().stream()
-            .skip(start)
-            .limit(end - start + 1)
-            .flatMap(e -> withScores
-                    ? Stream.of(e.getKey(), Slice.create(e.getValue().toString()))
-                    : Stream.of(e.getKey()))
-            .map(Response::bulkString)
-            .collect(Collectors.toList());
+                .sorted(isRev ? zRangeComparator.reversed() : zRangeComparator)
+                .skip(start)
+                .limit(end - start + 1)
+                .flatMap(e -> finalWithScores
+                        ? Stream.of(e.getKey(), Slice.create(e.getValue().toString()))
+                        : Stream.of(e.getKey()))
+                .map(Response::bulkString)
+                .collect(Collectors.toList());
 
         return Response.array(values);
     }
