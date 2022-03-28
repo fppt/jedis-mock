13c9ea307d28c473fefe70d4935b87b6233fcca1
Ivan Ponomarev <iponomarev@mail.ru>
2022-03-28 16:42:56 +0300

    refactor bitmap tests

--- src/test/java/com/github/fppt/jedismock/comparisontests/bitmaps/BitMapsOperationsTest.java
+++ src/test/java/com/github/fppt/jedismock/comparisontests/bitmaps/BitMapsOperationsTest.java
@@ -10,6 +10,7 @@ import redis.clients.jedis.Jedis;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,20 +18,38 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(ComparisonBase.class)
 public class BitMapsOperationsTest {
 
+    private final List<Integer> bits = Arrays.asList(2, 3, 5, 10, 11, 14);
+
     @BeforeEach
     public void setUp(Jedis jedis) {
         jedis.flushAll();
+        for (int i : bits) {
+            jedis.setbit("bm", i, true);
+        }
     }
 
     @TestTemplate
-    void testValueAftersetbit(Jedis jedis){
+    void testSetBitByBitValue(Jedis jedis) {
+        for (int i = 0; i <= Collections.max(bits); i++) {
+            assertEquals(bits.contains(i), jedis.getbit("bm", i));
+        }
+    }
+
+    @TestTemplate
+    void testGetStringRepresentation(Jedis jedis) {
+        String bitmapString = jedis.get("bm");
+        jedis.set("bm2", bitmapString);
+        for (int i = 0; i <= Collections.max(bits); i++) {
+            assertEquals(bits.contains(i), jedis.getbit("bm2", i));
+        }
+    }
+
+    @TestTemplate
+    void testValueAftersetbit(Jedis jedis) {
         jedis.setbit("foo", 0L, true);
         assertEquals(jedis.getbit("foo", 0L), true);
         jedis.setbit("foo", 1L, true);
         assertEquals(jedis.getbit("foo", 0L), true);
-        String str = jedis.get("foo");
-        jedis.set("hi", str);
-        assertEquals(jedis.getbit("hi", 0L), true);
-        assertEquals(jedis.getbit("hi", 1L), true);
     }
+
 }
