a5c3fc4b75a1f57587a165318c4edf70d61e0e31
DibrovDru <dibrov.aa@phystech.edu>
2022-05-03 17:50:09 +0300

    add RMHyperLogLog

--- src/main/java/com/github/fppt/jedismock/datastructures/RMHyperLogLog.java
+++ src/main/java/com/github/fppt/jedismock/datastructures/RMHyperLogLog.java
@@ -1,26 +1,56 @@
 package com.github.fppt.jedismock.datastructures;
 
+import com.github.fppt.jedismock.exception.SerializationException;
 import com.github.fppt.jedismock.exception.WrongValueTypeException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
-public class RMHLL implements RMDataStructure {
-    private Set<Slice> data;
+public class RMHyperLogLog implements RMDataStructure, Serializable {
+    private static final long serialVersionUID= 1L;
+    private final HashSet<Slice> storedData;
 
+    public RMHyperLogLog() {
+        storedData = new HashSet<>();
+    }
+
+    public Set<Slice> getStoredData() {
+        return storedData;
+    }
+
+    public int size() {
+        return storedData.size();
+    }
+
+    public void addAll(Collection<Slice> data) {
+        storedData.addAll(data);
+    }
 
     @Override
     public void raiseTypeCastException() {
-        throw new WrongValueTypeException("WRONGTYPE RMHLL value is used in the wrong place");
+        throw new WrongValueTypeException("WRONGTYPE RMHyperLogLog value is used in the wrong place");
     }
 
     @Override
     public String getTypeName() {
-        return null;
+        return "HyperLogLog";
     }
 
     @Override
     public Slice getAsSlice() {
-        return RMDataStructure.super.getAsSlice();
+        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(byteOutputStream);
+            outputStream.writeObject(this);
+            return Slice.create(byteOutputStream.toByteArray());
+        } catch (IOException exp) {
+            throw new SerializationException("problem with RMBitMap serialization");
+        }
     }
 
 
